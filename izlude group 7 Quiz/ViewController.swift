ab2bb3345075b6bb2620e20a7a8f8116f642c549
Novan Agung Waskito <68317534+jaygung17@users.noreply.github.com>
2022-07-28 17:10:23 +0700

    Initial Commit

---
+++ izlude group 7 Quiz/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  izlude group 7 Quiz
+//
+//  Created by Agung Luo on 28/07/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
